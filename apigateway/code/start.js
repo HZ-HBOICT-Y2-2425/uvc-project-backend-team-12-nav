ddae281dcd289ebf7302898c37c872d005fae525
brandonmura <144110401+brandonmura@users.noreply.github.com>
2024-12-11 17:31:23 +0100

    Update start.js

--- apigateway/code/start.js
+++ apigateway/code/start.js
@@ -1,21 +1,17 @@
 
 import express from 'express';
-<<<<<<< HEAD
 import * as dotenv from 'dotenv';
 import cors from 'cors'; // Import the cors package
-
 dotenv.config({ path: 'variables.env' });
 import indexRouter from './routes/index.js';
-=======
 import path from 'path';
 import { fileURLToPath } from 'url';
 import routes from './routes/index.js';
->>>>>>> feature/waterlogMicroservice
+
 
 const app = express();
 const PORT = process.env.PORT || 80;
 
-<<<<<<< HEAD
 // Enable CORS middleware
 app.use(cors());
 
@@ -25,14 +21,13 @@ app.use(express.urlencoded({ extended: true }));
 
 // Use the index router
 app.use('/', indexRouter);
-=======
+
 // Needed to get __dirname with ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Serve static files from the frontend build directory
 app.use(express.static(path.join(__dirname, 'public')));
->>>>>>> feature/waterlogMicroservice
 
 // Use the routes defined in routes/index.js
 app.use('/', routes);
